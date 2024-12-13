251ebd63402eef730cc3c594ce306887851d731e
Juan Mireles <juanmireles@proton.me>
2024-12-13 00:07:30 -0500

    Make a page to add exercises and starting defining the color pallete

---
+++ constants/Colors.ts
@@ -0,0 +1,10 @@
+export const Colors = {
+    // Base colors
+    primary: '#C96868',
+    background: '#121212',
+    surface: '#1E1E1E',
+    card: '#252525',
+
+    // Text colors
+    text: {},
+} as const;
